b07ea689bb94517f9bd08785fd1db10d9abdf65c
paul <paul.chazalmartin@protonmail.com>
2026-07-04 18:38:55 +0200

    ok

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,9 +1,24 @@
 return {
   "folke/flash.nvim",
+  event = "VeryLazy",
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- s : saut rapide (normal + visuel). PAS de mode "o" pour laisser
+    -- l'operator-pending libre (surround cs/ds/ys viendra en Step 12).
+    { "s", mode = { "n", "x" }, function() require("flash").jump() end, desc = "Flash jump" },
+    -- S : saut sur des nœuds Treesitter (sélection de blocs de code).
+    { "S", mode = { "n", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
   },
-  opts = {}
+  opts = {
+    label = {
+      uppercase = false, -- labels en minuscules, plus rapides à taper
+    },
+    modes = {
+      -- Ne PAS activer le "jump label" pendant une recherche /,
+      -- pour ne pas perturber ton flux de recherche habituel.
+      search = { enabled = false },
+      -- f/F/t/T restent le comportement Vim natif, mais Flash ajoute le
+      -- repeat amélioré (optionnel). On laisse activé, c'est peu intrusif.
+      char = { enabled = true },
+    },
+  },
 }
-
